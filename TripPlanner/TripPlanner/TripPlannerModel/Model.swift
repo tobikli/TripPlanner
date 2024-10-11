0be3ea8c8139e19aad97cf62cc4f753757b92b40
Tobias Klingenberg <tbs.kling@gmail.com>
2024-10-11 10:17:11 +0200

    Restructure File System

---
+++ TripPlanner/TripPlanner/TripPlannerModel/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  TripPlanner
+//
+//  Created by Tobias Klingenberg on 11.10.24.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: Model
+
+@Observable public class Model {
+    
+    // The model is the shareModelContainer created in TripPlannerApp.swift
+    
+}
